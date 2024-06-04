1419bac67634057afbc0f51a591f8b7c7683e48e
Mohammed Abdiladif <mihabulle45a@gmail.com>
2024-06-04 13:53:58 +0300

    Fixed Issue with image Path

--- Assigment1.py
+++ Assigment1.py
@@ -3,7 +3,7 @@ Assigment 1 Detect Faces from static Images Predict their Gender and the Age of 
 '''
 import cv2
 face_Classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml") # getting the face detection Model 
-img = cv2.imread('./Face-And-Gender-Detection-/Images/img1.jpg',1) 
+img = cv2.imread('./Images/img1.jpg',1) 
 grayScaleImage = cv2.cvtColor(img,0) #load same  image in Gray Scale
 faces = face_Classifier.detectMultiScale(grayScaleImage,1.2,2)
 for (x,y,w,h) in faces :
